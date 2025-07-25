eced10b36862331367c6dfd4a59bde34373b69a0
Vaibhav-Kumar-K-R <vaibhavkumar.k.r.cnr@gmail.com>
2025-07-25 21:19:10 +0530

    Formatting changes dome

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,28 +8,27 @@ import clipRouter from "./routes/clip.route";
 
 dotenv.config();
 
-
-
 const supabaseUrl = process.env.SUPABASE_URL as string;
 const supabaseKey = process.env.SUPABASE_KEY as string;
-const bucketName = process.env.SUPABASE_BUCKET || 'videos';
+const bucketName = process.env.SUPABASE_BUCKET || "videos";
 if (!supabaseUrl || !supabaseKey) {
-  throw new Error('Supabase credentials are needed');
+    throw new Error("Supabase credentials are needed");
 }
 const supabase = createClient(supabaseUrl, supabaseKey, {
-  auth: { persistSession: false }
+    auth: { persistSession: false },
 });
 
 const app = express();
 const port = process.env.PORT || 3001;
 
-const allowedOrigin = process.env.NODE_ENV === "production" 
-  ? "https://clippa.in" 
-  : "http://localhost:3000";
+const allowedOrigin =
+    process.env.NODE_ENV === "production"
+        ? "https://clippa.in"
+        : "http://localhost:3000";
 
 const corsOptions: cors.CorsOptions = {
-  origin: allowedOrigin,
-  credentials: true,
+    origin: allowedOrigin,
+    credentials: true,
 };
 
 app.use(cors(corsOptions));
@@ -38,26 +37,23 @@ app.use(express.json());
 const uploadsDir = path.join(__dirname, "../uploads");
 console.log(`Uploads directory: ${uploadsDir}`);
 if (!fs.existsSync(uploadsDir)) {
-  fs.mkdirSync(uploadsDir);
+    fs.mkdirSync(uploadsDir);
 }
 
 const jobsDir = path.join(__dirname, "../jobs");
 if (!fs.existsSync(jobsDir)) {
-  fs.mkdirSync(jobsDir);
+    fs.mkdirSync(jobsDir);
 }
 console.log(`Jobs directory: ${jobsDir}`);
 
-
 app.use("/api/clip", clipRouter);
 
-app.get('/api/ping', (_req, res) => {
-  return res.json({ success: true });
+app.get("/api/ping", (_req, res) => {
+    return res.json({ success: true });
 });
 
 app.get("/", (req, res) => res.send("Server is running!"));
 
-
-
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
